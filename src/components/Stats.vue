63b1714a08ccf939530a5a309af8b691828d7b71
helenxfu <helenxfubiz@gmail.com>
2019-07-03 14:23:45 +0900

    first commit

---
+++ src/components/Stats.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Stats</h2>
+    <p>{{countLinks}} links</p>
+    <button @click="removeAllLinks">Remove All</button>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'Stats',
+  computed: {
+    ...mapGetters(['countLinks'])
+  },
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks () {
+      this.removeAll().then(() => {
+        this.msg = 'removed'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'My custom title',
+    links: ['https://helenxfu.github.io/', 'https://twitter.com/HelenStarcloud', 'https://www.linkedin.com/in/helenxfu', 'https://github.com/helenxfu']
+  },
+  getters: {
+    countLinks: state => state.links.length
   },
   mutations: {
-
+    ADD_LINK: (state, link) => { state.links.push(link) },
+    REMOVE_LINK: (state, link) => { state.links.splice(link, 1) },
+    REMOVE_ALL: state => { state.links = [] }
   },
   actions: {
-
+    removeLink: (context, link) => {
+      context.commit('REMOVE_LINK', link)
+    },
+    removeAll ({ commit }) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
   }
 })
